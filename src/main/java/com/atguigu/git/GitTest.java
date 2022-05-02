d8b5e1224e19f3cd979655071f07a15174f57026
woaijiyu <103085551+woaijiyu@users.noreply.github.com>
2022-05-02 17:55:30 +0800

    commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,10 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        
+        for(int i=0; i<100;i++){
+        System.out.println(i);
+        }
 
-    }
+    
 }
