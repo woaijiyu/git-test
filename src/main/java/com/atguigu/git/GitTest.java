45c94b50dfd0b2205073cbf7fd3dfb5037842123
Jiyu <woaijiyu1999@163.com>
2022-05-02 15:08:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-05-02 14:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
