be670f504c1cd724af69afe61426bb23201b8b58
Jiyu <woaijiyu1999@163.com>
2022-05-02 15:29:51 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,8 +8,10 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("hello git4!");
-        System.out.println("hello git8!");
+        System.out.println("hello git2!");
+        System.out.println("hello git2!");
+        System.out.println("hello git2!");
+        System.out.println("hello!");
+
     }
 }
